cb5bd9778c0a8326ffec51a67ab677342d0cf799
avblackwell <alexvblackwell@gmail.com>
2023-05-09 01:02:58 -0400

    intialize

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Alex Blackwell",
+	url: "https://example.com/",
+	language: "en",
+	description: "I am writing about my experiences as a developer making his way in tech.",
+	author: {
+		name: "Alex Blackwell",
+		email: "alexvblackwell@gmail.com",
+		url: "https://example.com/about/"
+	}
+}
